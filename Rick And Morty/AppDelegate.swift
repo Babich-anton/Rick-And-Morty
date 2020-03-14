6afd1794f6cc877de305cae8030b95905837917a
Anton Babich <anton.babich@sqil.by>
2020-03-14 15:54:12 +0300

    Created simple application.

---
+++ Rick And Morty/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Rick And Morty
+//
+//  Created by Anton Babich on 14.03.2020.
+//  Copyright © 2020 SQILSOFT. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+}
