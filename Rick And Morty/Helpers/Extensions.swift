7710fe4597f26f3e264508dd71a02aa9e72622df
Anton Babich <anton.babich@sqil.by>
2020-03-16 08:55:09 +0300

    LOGIN Created extensions to hide keyboard, for reactive programming.

---
+++ Rick And Morty/Helpers/Extensions.swift
@@ -0,0 +1,54 @@
+//
+//  Extensions.swift
+//  Rick And Morty
+//
+//  Created by Anton Babich on 15.03.2020.
+//  Copyright © 2020 SQILSOFT. All rights reserved.
+//
+
+import Foundation
+import MaterialComponents
+import RxCocoa
+import RxSwift
+import UIKit
+
+extension Reactive where Base: UIViewController {
+    
+    var viewWillAppear: ControlEvent<Void> {
+        return controlEvent(for: #selector(UIViewController.viewWillAppear))
+    }
+    
+    var viewDidAppear: ControlEvent<Void> {
+        return controlEvent(for: #selector(UIViewController.viewDidAppear))
+    }
+    
+    var viewWillDisappear: ControlEvent<Void> {
+        return controlEvent(for: #selector(UIViewController.viewWillDisappear))
+    }
+    
+    var viewDidDisappear: ControlEvent<Void> {
+        return controlEvent(for: #selector(UIViewController.viewDidDisappear))
+    }
+    
+    private func controlEvent(for selector: Selector) -> ControlEvent<Void> {
+        return ControlEvent(events: sentMessage(selector).map { _ in })
+    }
+}
+
+extension UIViewController {
+    
+    func hideKeyboardWhenTappedAround() {
+        
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
+
+func showMessage(with text: String) {
+    MDCSnackbarManager.show(MDCSnackbarMessage(text: text))
+}
