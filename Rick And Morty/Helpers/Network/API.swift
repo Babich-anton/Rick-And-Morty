e100a434bd45294ffe175330d82cfcae8dbf80de
Anton Babich <anton.babich@sqil.by>
2020-03-21 00:50:54 +0300

    Character Created struct to get endpoint for API.

---
+++ Rick And Morty/Helpers/Network/API.swift
@@ -0,0 +1,18 @@
+//
+//  API.swift
+//  Rick And Morty
+//
+//  Created by Anton Babich on 20.03.2020.
+//  Copyright © 2020 SQILSOFT. All rights reserved.
+//
+
+import Foundation
+
+struct API {
+
+    static let ENDPOINT = "https://rickandmortyapi.com/api/"
+    
+    static let CHARACTER = "character/"
+    static let LOCATION = "location/"
+    static let EPISODE = "episode/"
+}
