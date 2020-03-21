e2ea5cd4b453719fe8ba1ae9097dbad6120a6f62
Anton Babich <anton.babich@sqil.by>
2020-03-21 23:43:40 +0300

    Character Added ability to load cell. Fixed problem with reusing cells.

--- Rick And Morty/Helpers/UI/Cells/Character/CharacterViewCell.swift
+++ Rick And Morty/Helpers/UI/Cells/Character/CharacterViewCell.swift
@@ -6,19 +6,23 @@
 //  Copyright © 2020 SQILSOFT. All rights reserved.
 //
 
+import AlamofireImage
 import UIKit
 
 class CharacterViewCell: UITableViewCell {
 
+    @IBOutlet weak var loadingActivityIndicator: UIActivityIndicatorView!
     @IBOutlet weak var characterImageView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var speciesLabel: UILabel!
 
     var character: Character! {
         didSet {
-            // TODO: add cache for images
             if let url = URL(string: character.image) {
-                characterImageView.load(url: url)
+                characterImageView.af.setImage(withURL: url) { response in
+                    self.characterImageView.isHidden = false
+                    self.loadingActivityIndicator.stopAnimating()
+                }
             }
             
             nameLabel.text = character.name
@@ -32,5 +36,13 @@ class CharacterViewCell: UITableViewCell {
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+
+        self.loadingActivityIndicator.startAnimating()
+        self.characterImageView.isHidden = true
+        self.characterImageView.image = nil
     }
 }
