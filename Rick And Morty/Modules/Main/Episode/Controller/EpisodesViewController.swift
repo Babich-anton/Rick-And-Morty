bdcbe63fa79422025170a7d43aeb303b4a1a2acc
Anton Babich <anton.babich@sqil.by>
2020-03-21 23:50:39 +0300

    Episode Added ability to load episodes and open episode details.

--- Rick And Morty/Modules/Main/Episode/Controller/EpisodesViewController.swift
+++ Rick And Morty/Modules/Main/Episode/Controller/EpisodesViewController.swift
@@ -6,25 +6,66 @@
 //  Copyright © 2020 SQILSOFT. All rights reserved.
 //
 
-import Foundation
+import RxCocoa
+import RxSwift
 import UIKit
+
+fileprivate let CELL_IDENTIFIER = "episode-cell"
 
 class EpisodesViewController: UITableViewController {
 
+    var viewModel: EpisodeViewModel!
+    var selectedDetailsViewModel: EpisodeDetailsViewModel?
+    
+    private let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let cell = UINib(nibName: "EpisodeViewCell", bundle: nil)
+        self.tableView.register(cell, forCellReuseIdentifier: CELL_IDENTIFIER)
+        self.tableView.dataSource = nil
+        self.tableView.delegate = self
         
+        self.setupBinding()
+    }
+    
+    private func setupBinding() {
+        
+        viewModel.episodes.bind(to: self.tableView.rx.items(cellIdentifier: CELL_IDENTIFIER)) { row, model, cell in
+            if let cell = cell as? EpisodeViewCell {
+                cell.episode = model
+            }
+        }.disposed(by: disposeBag)
+        
+        self.tableView.rx.modelSelected(Episode.self)
+            .map { $0 }
+            .subscribe({ [unowned self] model in
+                if let element = model.element {
+                    self.selectedDetailsViewModel = EpisodeDetailsViewModel(episode: element)
+                    self.performSegue(withIdentifier: "ShowEpisodeDetailsFromCharacters", sender: nil)
+                } else {
+                    showMessage(with: "Could not found model. Please, try again!")
+                }
+            }).disposed(by: disposeBag)
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return viewModel.episodes.value.count
     }
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "ShowEpisodeDetailsFromCharacters" {
+            if let vc = segue.destination as? EpisodeDetailsViewController {
+                vc.episodeViewModel = self.selectedDetailsViewModel
+            }
+        }
+    }
 }
