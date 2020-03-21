41fad0ceef790baa7509bfb4737273d743579435
Anton Babich <anton.babich@sqil.by>
2020-03-21 23:49:34 +0300

    Episode Created episode details view controller.

---
+++ Rick And Morty/Modules/Main/Episode/Controller/EpisodeDetailsViewController.swift
@@ -0,0 +1,28 @@
+//
+//  EpisodeDetailsViewController.swift
+//  Rick And Morty
+//
+//  Created by Anton Babich on 21.03.2020.
+//  Copyright © 2020 SQILSOFT. All rights reserved.
+//
+
+import UIKit
+
+class EpisodeDetailsViewController: UIViewController {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var episodeLabel: UILabel!
+    @IBOutlet weak var airDateLabel: UILabel!
+    
+    var episodeViewModel: EpisodeDetailsViewModel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let episode = episodeViewModel.episode.value
+        
+        nameLabel.text = episode.name
+        episodeLabel.text = episode.episode
+        airDateLabel.text = episode.airDate
+    }
+}
